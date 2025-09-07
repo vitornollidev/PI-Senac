d67b97c99cdc3ab57b24ea703b3754f439f42045
paulo-sagaz <122912285+paulo-sagaz@users.noreply.github.com>
2025-09-07 19:00:19 -0300

    Ultima alteracao

--- PI/app/config.py
+++ PI/app/config.py
@@ -1,9 +1,4 @@
 class Config:
-    # Senha
     SECRET_KEY = '123456'
-
-    # URI do banco de dados, nesse caso um banco de dados SQLite chamado site.db
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-
-    # Desativa o rastreamento de modificações do banco de dados, melhorando o desempenho
     SQLALCHEMY_TRACK_MODIFICATIONS = False
