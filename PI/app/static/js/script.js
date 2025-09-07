0992af79e047d71fc0fa0591db79e1dd047cc071
thayannecarrilho <thayanne.cscarrilho@senacsp.edu.br>
2025-09-07 09:41:55 -0300

    Update UI components and add financial image

--- PI/app/static/js/script.js
+++ PI/app/static/js/script.js
@@ -71,54 +71,6 @@ if (chartElement) {
 
 
 
-// Configuração modo escuro
-// Evento de carregamento de página para configurar o modo escuro
-document.addEventListener('DOMContentLoaded', function() {
-    // Seleciona os elementos necessários para o modo escuro
-    const toggleButton = document.getElementById('dark-mode-toggle');
-    const body = document.body;
-    const container = document.querySelector('.container');
-    const header = document.querySelector('.header');
-    const navLinks = document.querySelectorAll('.nav-link');
-    const buttons = document.querySelectorAll('button');
-    const inputs = document.querySelectorAll('input, select');
-    const tables = document.querySelectorAll('table, th, td');
-    const footer = document.querySelector('footer');
-
-    // Função para alternar o modo escuro
-    function toggleDarkMode() {
-        // Alterna a classe 'dark-mode' nos elementos selecionados
-        body.classList.toggle('dark-mode');
-        container.classList.toggle('dark-mode');
-        header.classList.toggle('dark-mode');
-        footer.classList.toggle('dark-mode');
-        
-        // Alterna a classe 'dark-mode' nos links de navegação
-        navLinks.forEach(link => link.classList.toggle('dark-mode'));
-        // Alterna a classe 'dark-mode' nos botões
-        buttons.forEach(button => button.classList.toggle('dark-mode'));
-        // Alterna a classe 'dark-mode' nos inputs
-        inputs.forEach(input => input.classList.toggle('dark-mode'));
-        // Alterna a classe 'dark-mode' nas tabelas
-        tables.forEach(table => table.classList.toggle('dark-mode'));
-
-        // Salva a preferência no localStorage
-        if (body.classList.contains('dark-mode')) {
-            localStorage.setItem('dark-mode', 'enabled');
-        } else {
-            localStorage.removeItem('dark-mode');
-        }
-    }
-
-    // Verifica se a preferência do modo escuro está salva no localStorage
-    if (localStorage.getItem('dark-mode') === 'enabled') {
-        // Alterna o modo escuro se a preferência estiver salva
-        toggleDarkMode();
-    }
-
-    // Adiciona o evento de clique ao botão de alternância do modo escuro
-    toggleButton.addEventListener('click', toggleDarkMode);
-});
 
 document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
